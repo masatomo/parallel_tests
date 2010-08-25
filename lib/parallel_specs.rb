9d0aa60794b063803897fd5b019042a2192320d1
Masatomo Nakano <masatomon@reallyenglish.com>
2010-08-25 14:32:30 +0100

    Replace <%= ENV['TEST_ENV_NUMBER'] %> with test number of parallel build in (parallel_)spec.opts . e.g. you can set "--format html:spec/spec-reports/index<%= ENV['TEST_ENV_NUMBER'] %>.html" in (parallel_)spec.opts

--- lib/parallel_specs.rb
+++ lib/parallel_specs.rb
@@ -1,8 +1,9 @@
 require File.join(File.dirname(__FILE__), 'parallel_tests')
+require 'erb'
 
 class ParallelSpecs < ParallelTests
   def self.run_tests(test_files, process_number, options)
-    cmd = "#{color} #{executable} #{options} #{spec_opts} #{test_files*' '}"
+    cmd = "#{color} #{executable} #{options} #{spec_opts(process_number)} #{test_files*' '}"
     execute_command(cmd, process_number)[:stdout]
   end
 
@@ -25,9 +26,18 @@ class ParallelSpecs < ParallelTests
     `#{cmd}`
   end
 
-  def self.spec_opts
+  def self.spec_opts(num)
     opts = ['spec/parallel_spec.opts', 'spec/spec.opts'].detect{|f| File.file?(f) }
-    opts ? "-O #{opts}" : nil
+    if opts
+      # TODO: setting ENV here is not good idea though...
+      ENV["TEST_ENV_NUMBER"] = num.to_s
+      parsed_opts = ERB.new(File.open(opts).read).result
+      f_name = ".parallel_spec#{num}.opts"
+      File.open(f_name, "w") {|f| f.write(parsed_opts) }
+      opts ? "-O #{f_name}" : nil
+    else
+      nil
+    end
   end
 
   #display color when we are in a terminal
